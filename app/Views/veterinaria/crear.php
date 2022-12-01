1c54176ee13242e9561c4c362c6492fd3296c817
Alexis001X <danielgato19991@hotmail.com>
2022-11-30 21:20:12 -0500

    Correcion de error:
    Se arregla el error en las cajas del formulario

--- app/Views/veterinaria/crear.php
+++ app/Views/veterinaria/crear.php
@@ -2,43 +2,39 @@
  echo view("plantillas/header");
 ?>
 <div class="container px-4 py-5" id="featured-3">
-    <h2 class="pb-2 border-bottom">Servico de registro de citas:</h2>
+    <h2 class="pb-2 border-bottom">Servico de registro de Reservaciones:</h2>
 <form action ="<?phP echo base_url()?>/veterinaria/guardar"method="POST" >
 <form class="row g-3">
   <div class="col-md-6">
-    <label for="Nombre" class="form-label">Nombre de la mascota:</label>
+    <label for="Nombre" class="form-label">Salas:</label>
     <input type="text" class="form-control" id="Nombre" name="Nombre" required>
   </div>
   <div class="col-md-6">
-    <label for="Especie" class="form-label">Especie:</label>
+    <label for="Especie" class="form-label">Horario:</label>
     <input type="text" class="form-control" id="Especie" name="Especie" required>
   </div>
   <div class="col-12">
-    <label for="Servicio" class="form-label">Servicio:</label>
+    <label for="Servicio" class="form-label">Disponibilidad:</label>
     <select class="form-select" aria-label="Default select example" id="Servicio" name="Servicio" required>
   <option selected>Selecciona:</option>
-  <option value="Control mensual">Control mensual</option>
-  <option value="Revision">Revicion</option>
-  <option value="Vacunacion">Vacunacion</option>
-  <option value="Esterilizacion">Esterilizacion</option>
-  <option value="Intervencion quirirurgica">Intervencion quirurgica</option>
-  <option value="Adopcion">Adopcion</option>
-  <option value="Sala de partos">Sala de partos</option>
+  <option value="Control mensual">Disponible</option>
+  <option value="Revision">Ocupada</option>
+  <option value="Vacunacion">En mantenimiento</option>
+  <option value="Esterilizacion">Fuera de servicio</option>
 </select>
   </div>
   <div class="col-12">
-    <label for="Servicio2" class="form-label">Otro servicio:</label>
+    <label for="Servicio2" class="form-label">Servicio de comida:</label>
     <select class="form-select" aria-label="Default select example" id="Servicio2" name="Servicio2" required> 
-  <option selected>Requisitos opcionales:</option>
-  <option value="Peluqueria">Peluqueria</option>
-  <option value="Aseo">Aseo</option>
-  <option value="Croquetas">Croquetas</option>
-  <option value="Servicio de paseaperros">Servicio de paseaperros</option>
-  <option value="Medicaciones">Medicaciones</option>
+  <option selected>Opciones:</option>
+  <option value="Peluqueria">Buffeth abierto</option>
+  <option value="Aseo">Menu personalizado</option>
+  <option value="Croquetas">Platillos Gourmeth</option>
+  <option value="Servicio de paseaperros">Comida tipica</option>
 </select>
   </div>
   <div class="col-md-6">
-    <label for="Dueño" class="form-label">Datos del dueño</label>
+    <label for="Dueño" class="form-label">Servicio de licores:</label>
     <input type="text" class="form-control" id="Dueño" name="Dueño" required>
   </div>
   <div class="col-md-6">
